f111629fb1298c777907593b8d4813395c680237
root <root@localhost.localdomain>
2024-03-27 07:26:23 +0530

    simple chat app using reverb and vite

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+#Route::get('/', function () {    return view('welcome');});
+
+
+
+use App\Http\Controllers\ChatController;
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('chat');
 });
+
+Route::post('/chat/send-message', [ChatController::class, 'store'])->name('chat.send');
